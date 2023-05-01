8c26da6e057268cd4437ae26f63096eb758d651d
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-01 13:49:36 +0300

    Add Entity model

---
+++ app/models/entity.rb
@@ -0,0 +1,5 @@
+class Entity < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :entities_groups, dependent: :destroy
+    has_many :groups, through: :entities_groups
+end
