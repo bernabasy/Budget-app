ea0bc12912f28f5e004c7ce8010d3e533f36615c
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-01 14:01:03 +0300

    Add validation for groups table

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :entities_groups, dependent: :destroy
   has_many :entities, through: :entities_groups
+
+  has_one_attached :icon
+  validates :name, :icon, presence: true
 end
