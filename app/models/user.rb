085bff51ad03b75f104587f61a42f8eb2d7d4bf2
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-02 15:12:44 +0300

    Add users models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+     # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-  validates :name, presence: true
+    devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :groups, foreign_key: :author_id
-  has_many :entities, foreign_key: :author_id
+has_many :groups, foreign_key: :author_id
+has_many :entities, foreign_key: :author_id
+
+# validates :name, :email, :password, presence: true
+
+validates :name, presence: true, length: { minimum: 0, maximum: 50 }
+
+
 end
