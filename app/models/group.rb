6baf9f4fcac4003fe507539aaf4cbc015c1257dc
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-02 15:12:58 +0300

    Add groups models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,9 @@
 class Group < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :entities_groups, dependent: :destroy
-  has_many :entities, through: :entities_groups
-
-  has_one_attached :icon
-  validates :name, :icon, presence: true
-end
+    belongs_to :author, class_name: 'User'
+    has_many :entities_groups, dependent: :destroy
+    has_many :entities, through: :entities_groups
+  
+    has_one_attached :icon
+    validates :name, :icon, presence: true
+  end
+  
