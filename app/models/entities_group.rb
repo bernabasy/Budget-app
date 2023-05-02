a7f46d913d99170e0d979bd7c8835ed3a83d1c39
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-02 19:08:59 +0300

    Add a total_purchases_group

--- app/models/entities_group.rb
+++ app/models/entities_group.rb
@@ -1,4 +1,9 @@
 class EntitiesGroup < ApplicationRecord
-  belongs_to :group
-  belongs_to :entity
-end
+    belongs_to :group
+    belongs_to :entity
+
+    after_create :total_purchases_group
+  def total_purchases_group
+    group.increment!(:total_purchases, purchase.amount)
+  end
+  end
