eff5d2370865ae165ab724bcf12105a14c714ec7
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-02 15:10:33 +0300

    Add entity models

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,8 +1,9 @@
 class Entity < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :entities_groups, dependent: :destroy
-  has_many :groups, through: :entities_groups
-
-  validates :name, presence: true
-  validates :amount, presence: true
-end
+    belongs_to :author, class_name: 'User'
+    has_many :entities_groups, dependent: :destroy
+    has_many :groups, through: :entities_groups
+  
+    validates :name, presence: true
+    validates :amount, presence: true
+  end
+  
