64c24e6745f674e62322bcaf35a3725ffa2db740
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-02 15:06:23 +0300

    Remove unused columns

--- db/migrate/20230501081512_add_references_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddReferencesToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :users, :entity, null: false, foreign_key: true
-    add_reference :users, :group, null: false, foreign_key: true
-  end
-end
