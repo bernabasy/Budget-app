f611d9a4561b0ce5f6555c860f641460621a39a0
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-03 20:27:19 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :entities
-  resources :groups
   devise_for :users
   resources :users
+
+  resources :groups, only: [:index, :new, :create, :destroy] do
+    resources :entities, only: [:index, :new, :create, :destroy]
+ end           
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
