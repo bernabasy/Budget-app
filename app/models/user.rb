b2b1c16bc10a0672771f6af418f8b47499a97a29
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-03 20:24:39 +0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ApplicationRecord
-     # Include default devise modules. Others available are:
+  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
 
-has_many :groups, foreign_key: :author_id
-has_many :entities, foreign_key: :author_id
+  has_many :groups, foreign_key: :author_id
+  has_many :entities, foreign_key: :author_id
 
-# validates :name, :email, :password, presence: true
+  # validates :name, :email, :password, presence: true
 
-validates :name, presence: true, length: { minimum: 0, maximum: 50 }
-
-
+  validates :name, presence: true, length: { minimum: 0, maximum: 50 }
 end
