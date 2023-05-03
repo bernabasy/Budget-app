d5d6eac3f6632139db04fe4bd4e7298713102da1
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-03 20:25:54 +0300

    Remove un used table

--- app/models/entities_group.rb
+++
@@ -1,9 +0,0 @@
-class EntitiesGroup < ApplicationRecord
-    belongs_to :group
-    belongs_to :entity
-
-    after_create :total_purchases_group
-  def total_purchases_group
-    group.increment!(:total_purchases, purchase.amount)
-  end
-  end
