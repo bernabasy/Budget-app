e1e1e266c95cedd13c917e44802484680078426f
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-03 20:26:27 +0300

    Update migrations

--- db/migrate/20230501080837_create_entities.rb
+++ db/migrate/20230501080837_create_entities.rb
@@ -4,7 +4,6 @@ class CreateEntities < ActiveRecord::Migration[7.0]
       t.integer :author_id
       t.string :name
       t.float :amount
-      t.references :user, null: false, foreign_key: true, index: true
       
       t.timestamps
     end
