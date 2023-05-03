2cc3b064504d16f8ccc55bd2993b5cfc36d8b0f2
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-03 20:24:23 +0300

    Update entity model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,9 +1,8 @@
 class Entity < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :entities_groups, dependent: :destroy
-    has_many :groups, through: :entities_groups
-  
-    validates :name, presence: true
-    validates :amount, presence: true
-  end
-  
+  belongs_to :author, class_name: 'User'
+  has_many :group_entities, dependent: :destroy
+  has_many :groups, through: :group_entities
+
+  validates :name, presence: true
+  validates :amount, presence: true
+end
