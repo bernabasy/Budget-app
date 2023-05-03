aa3672c8a143025e8d244eeef87df2dbaa8fd7be
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-03 20:24:53 +0300

    Update user model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,11 @@
 class Group < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :entities_groups, dependent: :destroy
-    has_many :entities, through: :entities_groups
-  
-    has_one_attached :icon
-    validates :name, :icon, presence: true
-  end
+  belongs_to :author, class_name: 'User'
+  attribute :user_id, :integer
+
+  has_many :group_entities, dependent: :destroy
+  has_many :entities, through: :group_entities
+
+
+  has_one_attached :icon
+  validates :name, :icon, presence: true
+end
